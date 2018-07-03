b467af9daa061965c0cc7d5c5c52d75ed47ec495
Yatendra Singh <yatendra1999luffy@gmail.com>
2018-07-03 10:58:31 +0530

    basic dictionary

--- lol/dictionary/urls.py
+++ lol/dictionary/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:string>/', views.word, name='word'),
+    path('<str:string>/', views.word, name ='word'),
+    path('',views.home, name = 'home'),
+    path('<str:string>/confirm', views.confirm, name = 'confirm')
 ]
--- lol/dictionary/views.py
+++ lol/dictionary/views.py
@@ -1,24 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import json
 from difflib import get_close_matches
 import os
 
-def translate(w, data):
-    w = w.lower()
-    if w in data:
-        return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        yn = get_close_matches(w, data.keys())[0]
-        return data[yn]
-    else:
-        return "The word doesn't exist. Please double check it."
-
+data = json.load(open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'data.json')))
 
 # Create your views here.
 
 def word(request, string):
-    BS_DIR = os.path.dirname(os.path.abspath(__file__))
-    data = json.load(open(os.path.join(BS_DIR,'data.json')))
-    shits = translate(string, data)
-    print(shits)
-    return render(request,'word.html',{'lol':shits})
+    string = string.lower()
+    shits = data[string]
+    return render(request,'word.html',{'shits':shits})
+
+def confirm(request, string):
+    if request.method == 'POST':
+        ans = request.POST.get('ans')
+        if(ans.lower()=='y'):
+            return redirect(word, string = string)
+        else:
+            return render(request,'error.html')
+    else:
+        return render(request, 'confirm.html', {'string' : string})
+
+def home(request):
+    if request.method == 'POST':
+        inp = request.POST.get('word')
+        print(type(inp))
+        inp = inp.lower()
+        if inp in data.keys():
+            return redirect(word, string = inp)
+        elif len(get_close_matches(inp, data.keys())) > 0:
+            return redirect(confirm, string = get_close_matches(inp, data.keys())[0])
+        else:
+            return render(request, 'error.html')
+    else:
+        return render(request, 'index.html')
